150ba05a00940283b37daf60af99b5991214823e
alexandrapiterova <alexandra.piterova@gmail.com>
2021-02-24 22:28:40 +0300

    added button design and initialization, top menu design

--- app/src/main/java/com/piterova/sfpm17_filmfinder/MainActivity.kt
+++ app/src/main/java/com/piterova/sfpm17_filmfinder/MainActivity.kt
@@ -2,10 +2,28 @@ package com.piterova.sfpm17_filmfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+        menu_button.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        favs_button.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+
+        watchlater_button.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
     }
 }
